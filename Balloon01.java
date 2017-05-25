0263087dba85e790c27fca7255b7ba784a6ff531
dh51x8cq <sasa-kouichirou-cb@ynu.jp>
2017-05-25 16:45:05 +0900

    Add files via upload

--- Balloon01.java
+++ Balloon01.java
@@ -1,13 +1,13 @@
 public class Balloon01{
-public static void main(String args[]){;
-int w=18;
-int x=4;
-int y=2;
-int z=5;
-System.out.println(w*x/(y+z));
-System.out.println(x-y-z);
-System.out.println(x*y*z);
-System.out.println(x/y/z);
-System.out.println(x/y+z);
-}
+    public static void main(String args[]){;
+        int w=9;
+        int x=3;
+        int y=7;
+        int z=5;
+	System.out.println(w-x+y+z);
+	System.out.println(x-y-z);
+	System.out.println(x*y*z);
+	System.out.println(x/y/z);
+	System.out.println(x/y+z);
+    }
 }
